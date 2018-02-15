46d7dd0feaee66890922ce524a8e24b48f72d98b
Thang Le Quoc <silverhair.guy@gmail.com>
2018-02-15 23:36:59 +0700

    init commit

---
+++ src/main/java/com/thanglequoc/SpringBootWildflyApplication.java
@@ -0,0 +1,12 @@
+package com.thanglequoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWildflyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWildflyApplication.class, args);
+	}
+}
