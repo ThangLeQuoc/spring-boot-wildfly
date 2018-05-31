0116c9fb82a4caa3e6dfc66a31af4501d64f4059
Thang Le Quoc <silverhair.guy@gmail.com>
2018-05-31 08:20:05 +0700

    WIP Convert to query by Criteria API

--- src/main/java/com/thanglequoc/departments/DepartmentService.java
+++ src/main/java/com/thanglequoc/departments/DepartmentService.java
@@ -4,7 +4,11 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
+import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -34,8 +38,16 @@ public class DepartmentService {
     }
 
     public DepartmentDto getDepartmentById(String id) {
+        /*
         DepartmentEntity departmentEntity = getDepartmentEntityById(id);
         return departmentHelper.convertToDto(departmentEntity);
+        */
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<DepartmentEntity> cq = cb.createQuery(DepartmentEntity.class);
+        Root<DepartmentEntity> departmentRoot = cq.from(DepartmentEntity.class);
+        cq.select(departmentRoot).where(cb.equal(departmentRoot.get(DepartmentEntity_.NUMBER), id ));
+        List<DepartmentEntity> result = em.createQuery(cq).getResultList();
+        return CollectionUtils.isNotEmpty(result) ? departmentHelper.convertToDto(result.get(0)) : null;
     }
 
 }
