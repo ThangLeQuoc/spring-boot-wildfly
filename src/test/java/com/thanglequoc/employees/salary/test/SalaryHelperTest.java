0d22385050e424ffdbab6ab29384ccab07e6b417
Thang Le Quoc <silverhair.guy@gmail.com>
2018-07-31 07:57:13 +0700

    Revert To Embedded Container Build
    - Problem: Cannot instantiate test

--- src/test/java/com/thanglequoc/employees/salary/test/SalaryHelperTest.java
+++ src/test/java/com/thanglequoc/employees/salary/test/SalaryHelperTest.java
@@ -11,14 +11,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.thanglequoc.SpringBootWildflyApplication;
+import com.thanglequoc.SpringRollApplication;
 import com.thanglequoc.employees.salary.SalaryDto;
 import com.thanglequoc.employees.salary.SalaryEntity;
 import com.thanglequoc.employees.salary.SalaryHelper;
 
-@SpringBootTest(classes = SpringBootWildflyApplication.class)
+@SpringBootTest(classes = SpringRollApplication.class, webEnvironment = WebEnvironment.NONE)
 @RunWith(SpringRunner.class)
 public class SalaryHelperTest {
 
